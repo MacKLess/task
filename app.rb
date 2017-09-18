e32511949521eabb64ab1db3585dacc567d6d7f7
Kelsey Langlois and LDMacKrell <ldmackrell@>
2017-09-18 13:45:00 -0700

    Update to display deadline date of tasks.

--- app.rb
+++ app.rb
@@ -4,8 +4,9 @@ also_reload('lib/**/*.rb')
 require('./lib/task')
 require('./lib/list')
 require('pg')
+require "pry"
 
-DB = PG.connect({:dbname => "to_do"})
+DB = PG.connect({:dbname => "to_do_test"})
 
 get('/') do
   erb(:index)
@@ -30,4 +31,18 @@ end
 get('/lists/:id') do
   @list = List.find(params.fetch(:id).to_i)
   erb(:list)
+end
+
+post('/tasks') do
+  description = params.fetch('description')
+  due_date = params.fetch('due-date')
+  if due_date.length == 0
+    due_date = "2017-12-31"
+  end
+  list_id = params.fetch('list_id').to_i
+  binding.pry
+  @list = List.find(list_id)
+  @task = Task.new({:description => description, :list_id => list_id, :due_date => due_date})
+  @task.save
+  erb(:task_success)
 end
