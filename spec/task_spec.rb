673ebd6f3d785c4b42a7f4aa5309ffa52365f0bf
LD MacKrell <ldmackrell@gmail.com>
2017-09-17 23:08:51 -0700

    add multiple tests and a spec_helper file.

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,6 +1,4 @@
-require('rspec')
-require('pg')
-require('task')
+require('spec_helper')
 
 DB = PG.connect({dbname => 'to_do_test'})
 
@@ -11,27 +9,50 @@ RSpec.configure do |config|
 end
 
 describe(Task) do
-  describe('#==') do
-    it('is the same task if it has the same description') do
-      task1 = Task.new({:description => "learn SQL"})
-      task2 = Task.new({:description => "learn SQL"})
-      expect(task1).to(eq(task2))
+#   describe('#==') do
+#     it('is the same task if it has the same description') do
+#       task1 = Task.new({:description => "learn SQL"})
+#       task2 = Task.new({:description => "learn SQL"})
+#       expect(task1).to(eq(task2))
+#     end
+#   end
+# end
+
+  describe('.all') do
+    it('it is empty at first') do
+      expect(Task.all()).to(eq([]))
     end
   end
-end
 
-describe('.all') do
-  it('it is empty at first') do
-    expect(Task.all()).to(eq([]))
+  describe Task do
+    describe('#save') do
+      it('adds a task to the array of saved tasks') do
+        test_task = Task.new({:description => "learn SQL", :list_id => 1})
+        test_task.save()
+        expect(Task.all()).to(eq([test_task]))
+      end
+    end
   end
-end
 
-describe Task do
-  describe('#save') do
-    it('adds a task to the array of saved tasks') do
-      test_task = Task.new({:description => "learn SQL"})
-      test_task.save()
-      expect(Task.all()).to(eq([test_task]))
+  describe('#description') do
+    it('lets you read the description out') do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.description()).to(eq("learn SQL"))
+    end
+  end
+
+  describe('#list_id') do
+    it('lets you read the list ID out') do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.list_id()).to(eq(1))
+    end
+  end
+
+  describe('#==') do
+    it('is the same task if it has the same description and list ID') do
+      task1 = Task.new({:description => "learn SQL", :list_id => 1})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(task1).to(eq(task2))
     end
   end
 end
